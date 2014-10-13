390833b25d6f88efd5909649badfc2350db9fe94
Wallouf <ct13860@gmail.com>
2014-10-13 16:10:37 -0400

    Suivi du tutoriel Spring de Developpez

---
+++ src/main/java/com/developpez/rpouiller/controller/BonjourController.java
@@ -0,0 +1,22 @@
+package com.developpez.rpouiller.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping( "/bonjour" )
+public class BonjourController {
+
+    @RequestMapping( method = RequestMethod.GET )
+    public String afficherBonjour( final ModelMap pModel,
+            @RequestParam( value = "personne" ) final String pPersonne,
+            @RequestParam( value = "test" ) final String pTest ) {
+
+        pModel.addAttribute( "personne", pPersonne );
+        pModel.addAttribute( "test", pTest );
+        return "bonjour";
+    }
+}
---
+++ src/main/webapp/vues/bonjour.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" isELIgnored="false"
+    pageEncoding="UTF-8"%>
+<%@taglib uri="http://www.springframework.org/tags" prefix="spring"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+    <head>
+        <title><spring:message code="titre.bonjour"/> : ${personne}</title>
+    </head>
+    <body>
+        <spring:message code="libelle.bonjour.lemonde" arguments="${personne}"/><br />
+       	Valeur de test : ${ test }
+    </body>
+</html>
